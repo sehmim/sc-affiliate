7d75c9f80532c1a79a814a835e93c52b50d52d32
sehmim <sehmim2010@hotmail.com>
2024-09-30 18:24:26 -0400

    wip

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -2,52 +2,62 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const fs = require('fs');
+const { fetchHostPermissions } = require('./src/utils/buildHelper');
+const { urlGetSyncedCampaigns } = require('./src/utils/env');
 
 // Function to generate manifest.json dynamically
 function createManifest() {
-  const manifest = {
-    manifest_version: 3,
-    name: "Shop for Good",
-    version: "1.7",
-    description: "Tracks brands that give you discounts. When You Shop. You Save. Charities and Causes Win.",
-    permissions: ["cookies", "storage"],
-    action: {
-      default_popup: "./popup.html"
-    },
-    content_scripts: [
-      {
-        matches: ["http://*/*", "https://*/*"],
-        js: ["content.js"],
-        css: ["css/index.css"]
-      }
-    ],
-    externally_connectable: {
-      matches: ["*://localhost/*", "*://sc-affiliate.vercel.app/*"]
-    },
-    web_accessible_resources: [
-      {
-        resources: ["css/index.css"],
-        matches: ["http://*/*", "https://*/*"]
-      }
-    ],
-    background: {
-      service_worker: "background.js"
-    },
-    icons: {
-      "16": "icons/icon.png",
-      "48": "icons/icon.png",
-      "128": "icons/icon.png"
-    },
-    host_permissions: ["*://*.example.com/*"]
-  };
 
-  // Write manifest.json to a temporary directory (build/)
-  fs.writeFileSync(path.resolve(__dirname, 'build', 'manifest.json'), JSON.stringify(manifest, null, 2), 'utf-8');
-  console.log('Manifest created successfully in build directory');
+  try {
+    const domains = fetchHostPermissions(urlGetSyncedCampaigns);
+
+    const manifest = {
+      manifest_version: 3,
+      name: "Shop for Good",
+      version: "1.7",
+      description: "Tracks brands that give you discounts. When You Shop. You Save. Charities and Causes Win.",
+      permissions: ["cookies", "storage"],
+      action: {
+        default_popup: "./popup.html"
+      },
+      content_scripts: [
+        {
+          matches: ["http://*/*", "https://*/*"],
+          js: ["content.js"],
+          css: ["css/index.css"]
+        }
+      ],
+      externally_connectable: {
+        matches: ["*://localhost/*", "*://sc-affiliate.vercel.app/*"]
+      },
+      web_accessible_resources: [
+        {
+          resources: ["css/index.css"],
+          matches: ["http://*/*", "https://*/*"]
+        }
+      ],
+      background: {
+        service_worker: "background.js"
+      },
+      icons: {
+        "16": "icons/icon.png",
+        "48": "icons/icon.png",
+        "128": "icons/icon.png"
+      },
+      host_permissions: domains
+    };
+
+    console.log('process', process.env)
+
+    fs.writeFileSync(path.resolve(__dirname, 'build', 'manifest.json'), JSON.stringify(manifest, null, 2), 'utf-8');
+    console.log('Manifest created successfully in build directory');  
+  } catch (error) {
+    console.log('error:', console.log(error))
+  }
 }
 
 module.exports = {
-  mode: 'development', // Change to 'production' for production builds
+  mode: 'production',
   entry: {
     content: path.join(__dirname, 'src', 'content', 'index.ts'),
     background: path.join(__dirname, 'src', 'background', 'background.js'),
@@ -92,8 +102,8 @@ module.exports = {
     {
       // Webpack hook to create the manifest.json file before the build starts
       apply: (compiler) => {
-        compiler.hooks.beforeRun.tap('CreateManifestPlugin', (compilation) => {
-          createManifest(); // Call the manifest creation function
+        compiler.hooks.beforeRun.tapPromise('CreateManifestPlugin', async (compilation) => {
+          await createManifest(); // Call the manifest creation function and wait for it to complete
         });
       },
     },
