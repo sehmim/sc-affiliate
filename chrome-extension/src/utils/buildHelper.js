161d084133def33f43b43ec32fb0f0267cea8f7b
sehmim <sehmim2010@hotmail.com>
2024-10-01 03:22:39 -0400

    fix bundle

--- chrome-extension/src/utils/buildHelper.js
+++ chrome-extension/src/utils/buildHelper.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { urlGetSyncedCampaigns } = require('./env');
 
 async function fetchHostPermissions(apiUrl) {
   try {
@@ -21,64 +22,53 @@ async function fetchHostPermissions(apiUrl) {
   }
 }
 
-async function createManifest(apiUrl) {
-  const hostPermissions = await fetchHostPermissions(apiUrl);
+async function createManifest() {
 
-  const manifest = {
-    manifest_version: 3,
-    name: "Shop for Good",
-    version: "1.61",
-    description: "Tracks brands that give you discounts. When You Shop. You Save. Charities and Causes Win.",
-    permissions: ["cookies", "storage"],
-    action: {
-      default_popup: "./popup/popup.html"
-    },
-    content_scripts: [
-      {
-        matches: [
-          "http://*/*",
-          "https://*/*"
-        ],
-        js: ["../../dist/content.js"],
-        css: ["../../css/index.css"]
-      }
-    ],
-    externally_connectable: {
-      matches: [
-        "*://localhost/*",
-        "*://sc-affiliate.vercel.app/*"
-      ]
-    },
-    web_accessible_resources: [
-      {
-        resources: ["../../css/index.css"],
-        matches: [
-          "http://*/*", 
-          "https://*/*"
-        ]
-      }
-    ],
-    background: {
-      service_worker: "../background/background.js"
-    },
-    icons: {
-      "16": "../../icons/icon.png",
-      "48": "../../icons/icon.png",
-      "128": "../../icons/icon.png"
-    },
-    host_permissions: hostPermissions,
-    content_security_policy: {
-      extension_pages: "script-src 'self'; object-src 'self'",
-      sandbox: "sandbox allow-scripts; script-src 'self' https://www.gstatic.com/ https://*.firebaseio.com https://www.googleapis.com"
-    }
-  };
+  try {
+    const domains = await fetchHostPermissions(urlGetSyncedCampaigns);
 
-  console.log('Manifest generated successfully!');
-  // return manifest;
-  // Write the manifest to a JSON file
-  fs.writeFileSync(path.resolve(__dirname, '../../manifest.json'), JSON.stringify(manifest, null, 2), 'utf-8');
+    const manifest = {
+      manifest_version: 3,
+      name: "Shop for Good",
+      version: "1.7",
+      description: "Tracks brands that give you discounts. When You Shop. You Save. Charities and Causes Win.",
+      permissions: ["cookies", "storage"],
+      action: {
+        default_popup: "./popup.html"
+      },
+      content_scripts: [
+        {
+          matches: ["http://*/*", "https://*/*"],
+          js: ["content.js"],
+          css: ["css/index.css"]
+        }
+      ],
+      externally_connectable: {
+        matches: ["*://localhost/*", "*://sc-affiliate.vercel.app/*"]
+      },
+      web_accessible_resources: [
+        {
+          resources: ["css/index.css"],
+          matches: ["http://*/*", "https://*/*"]
+        }
+      ],
+      background: {
+        service_worker: "background.js"
+      },
+      icons: {
+        "16": "icons/icon.png",
+        "48": "icons/icon.png",
+        "128": "icons/icon.png"
+      },
+      host_permissions: domains
+    };
+
+    fs.writeFileSync(path.resolve(__dirname, '../../manifest.json'), JSON.stringify(manifest, null, 2), 'utf-8');
+    console.log('Manifest created successfully in build directory');  
+  } catch (error) {
+    console.log('error:', console.log(error))
+  }
 }
-
 
 module.exports = {
   fetchHostPermissions,
