773d5089cc8ca991e14e98b3f2befa2a38aa5d08
sehmim <sehmim2010@hotmail.com>
2024-10-04 15:56:27 -0400

    change envs

--- chrome-extension/src/utils/env.js
+++ chrome-extension/src/utils/env.js
@@ -1,4 +1,4 @@
-const LOCAL_ENV = true;
+const LOCAL_ENV = false;
 
 const urlGetSyncedCampaigns = LOCAL_ENV ? "http://127.0.0.1:5001/sponsorcircle-3f648/us-central1/getSyncedCampaigns" : "https://us-central1-sponsorcircle-3f648.cloudfunctions.net/getSyncedCampaigns"; 
 
