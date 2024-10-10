daaa6161a37a03700f1f83dd7a3e2de54b028404
sehmim <sehmim2010@hotmail.com>
2024-10-09 21:01:52 -0400

    pushing 1.73

--- chrome-extension/src/utils/buildHelper.js
+++ chrome-extension/src/utils/buildHelper.js
@@ -30,7 +30,7 @@ async function createManifest() {
     const manifest = {
       manifest_version: 3,
       name: "Shop for Good",
-      version: "1.7",
+      version: "1.73",
       description: "Tracks brands that give you discounts. When You Shop. You Save. Charities and Causes Win.",
       permissions: ["cookies", "storage"],
       action: {
